88bf317f525f383e917de482ef8408330740c9f7
anthonyhem <anthony.rattana.hem@gmail.com>
2019-06-18 16:37:00 -0700

    Updated files to include lodash and handling api requests

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,11 @@
 import JsonPlaceholder from '../apis/jsonPlaceholder';
+import _ from 'lodash';
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+	await dispatch(fetchPosts());
+	const userIds = _.uniq(_.map(getState().posts, 'userId'));
+	userIds.forEach(id => dispatch(fetchUser(id)));
+}
 
 export const fetchPosts = () => async (dispatch) => {
 		const response = await JsonPlaceholder.get('/posts');
@@ -9,11 +16,21 @@ export const fetchPosts = () => async (dispatch) => {
 		});
 	};
 
-export const fetchUser = (id) => async (dispatch) => {
-	const response = await JsonPlaceholder.get(`/users/${id}`);
+//returns a function that returns fetchUser called
+// export const fetchUser = (id) => (dispatch) => _fetchUser(id,dispatch);
 
-	dispatch({
-		type: 'FETCH_USER',
-		payload: response.data
-	})
+// //this underscore tells others that this is a function to only be 
+// //called internally 
+// const _fetchUser = _.memoize(async(id, dispatch) => {
+// 	const response = await JsonPlaceholder.get(`/users/${id}`);
+
+// 	dispatch({
+// 		type: 'FETCH_USER',
+// 		payload: response.data
+// 	})
+// });
+
+export const fetchUser = id => async dispatch => {
+	const response = await JsonPlaceholder.get(`/users/${id}`);
+	dispatch({ type: 'FETCH_USER', payload: response.data });
 }
